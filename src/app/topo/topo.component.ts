279253a6aeb2b5a48efe8e7c669f159ed86fbfad
Jpprignolato <joseprignolato@gmail.com>
2025-01-17 17:33:26 -0300

    Criação dos componentes

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topo',
+  standalone: false,
+  
+  templateUrl: './topo.component.html',
+  styleUrl: './topo.component.css'
+})
+export class TopoComponent {
+
+}
